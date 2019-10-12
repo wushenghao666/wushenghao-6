d8a1006141b08658939d031aad589798d3732926
wushenghao666 <55724610+wushenghao666@users.noreply.github.com>
2019-10-12 23:23:08 +0800

    第一次的想法，最短路径存在问题
    不知道问题出在哪

---
+++ 1-有问题的深搜.cpp
@@ -0,0 +1,56 @@
+ #include <iostream>
+ #include <cstdio>
+ #include <stack>
+ #include <cmath>
+ #include <cstring>
+ using namespace std;
+ int migong[12][12];
+ int counts = 0, min_counts=100000;//counts为路径长度
+ 
+ int ways( int Sx, int Sy, int Fx, int Fy )//函数返回值是路径数量
+ {
+ 	counts ++;
+ 	if( Sx == Fx && Sy == Fy )
+ 		{if(counts<min_counts)
+ 			min_counts=counts;
+		 return 1;
+		 }
+	 //到达终点处理，也是函数的终止条件
+	migong[Sx][Sy] = 1;
+	 //表示我来过啦！
+	int num = 0;
+	 //用以记录路径总数的变量
+	if(!migong[Sx][Sy-1])
+		num += ways(Sx, Sy-1, Fx, Fy);
+	if(!migong[Sx][Sy+1])
+		num += ways(Sx, Sy+1, Fx, Fy);
+	if(!migong[Sx+1][Sy])
+		num += ways(Sx+1, Sy, Fx, Fy);
+	if(!migong[Sx-1][Sy])
+		num += ways(Sx-1, Sy, Fx, Fy);
+	 
+	migong[Sx][Sy] = 0;
+	counts --;
+	return num;
+ }
+ 
+ int main()
+ {
+ 	int n, m, Sx, Sy, Fx, Fy;
+ 	cin >> n >> m >> Sx >> Sy >> Fx >> Fy;
+	 //输入数据
+	for (int i=0; i<=11; i++)
+		for(int j=0; j<=11; j++)
+			migong[i][j]=1;
+	 //为迷宫砌墙
+ 	for (int i=1; i<=n; i++)
+ 		for(int j=1; j<=m; j++)
+ 			cin >> migong[i][j];
+	int way;
+	way = ways(Sx, Sy, Fx, Fy);
+	cout << way << endl ;
+	if(way) cout << min_counts;
+	else cout << 0;
+	 //异常处理
+ 	return 0;
+ }
